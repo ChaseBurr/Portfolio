0d1c8d318951c991b4b602edaed6390421d3bef3
Chase <chase@chaseburr.com>
2021-01-20 23:01:15 -0700

    updated project files

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -3,32 +3,32 @@ import "./Navbar.css";
 import { Link } from "react-router-dom";
 
 const Navbar = () => {
-  return (
-    <>
-      <nav>
-        <Link to="/" className="nav-logo">
-          Chase Burr
-        </Link>
-        <ul className="nav-items">
-          <Link to="/" className="nav-item">
-            Home
-          </Link>
-          <Link to="/projects" className="nav-item">
-            Projects
-          </Link>
-          <Link to="/resume" className="nav-item">
-            Resume
-          </Link>
-          <Link to="/photography" className="nav-item">
-            Contact
-          </Link>
-          <Link to="/photography" className="nav-item">
-            Photography
-          </Link>
-        </ul>
-      </nav>
-    </>
-  );
+   return (
+      <>
+         <nav>
+            <Link to="/" className="nav-logo">
+               Chase Burr
+            </Link>
+            <ul className="nav-items">
+               <Link to="/" className="nav-item">
+                  Home
+               </Link>
+               <Link to="/projects" className="nav-item">
+                  Projects
+               </Link>
+               <Link to="/resume" className="nav-item">
+                  Resume
+               </Link>
+               <Link to="/photography" className="nav-item">
+                  Contact
+               </Link>
+               <Link to="/photography" className="nav-item">
+                  Photography
+               </Link>
+            </ul>
+         </nav>
+      </>
+   );
 };
 
 export default Navbar;
