285b8e9fbc62b067a7e4fdff588d6011966ff7ab
Chase <chase@chaseburr.com>
2021-01-20 22:20:03 -0700

    updated styling

--- src/components/Pages/Home.js
+++ src/components/Pages/Home.js
@@ -6,7 +6,20 @@ import Footer from "./../Footer";
 function Home() {
    return (
       <>
-         <Hero />
+         {/* <Hero /> */}
+         <div className="about-container">
+            <div className="about-content">
+               <h2>Software Engineer</h2>
+               <p>
+                  Computer Science graduate seeking to improve my skills as a
+                  software engineer. Strong foundation in Object Oriented
+                  programming and troubleshooting. Quick learner and ability to
+                  adapt to new languages. I may not have much experience job
+                  wise, but I will dedicate a lot of time learning and adapting
+                  to new environments.
+               </p>
+            </div>
+         </div>
          <div className="container">
             <div className="recent-project-section">
                <h1 className="section-header">Projects in development</h1>
@@ -29,24 +42,52 @@ function Home() {
                   />
                </ul>
             </div>
+            <div className="recent-project-section">
+               <h1 className="section-header">All Projects</h1>
+               <ul className="recent-projects">
+                  <Project
+                     imgSrc="/images/Netflix-Clone.png"
+                     imgAlt="Netflix Clone"
+                     projectName="Netflix Clone"
+                     projectDescription="A clone of Netflix build with ReactJS"
+                     link="https://chaseburr.github.io/Netflix-Clone/#/"
+                     language="ReactJS"
+                  />
+                  <Project
+                     imgSrc="/images/Entertainment-App.png"
+                     imgAlt="Entertainment App"
+                     projectName="Entertainment App"
+                     projectDescription="Users can search for any Movie/TV Show and they will get a list of shows to watch related to their search."
+                     link="https://chaseburr.github.io/Entertainment-App/"
+                     language="ReactJS"
+                  />
+                  <Project
+                     imgSrc="/images/Athletic-shop.png"
+                     imgAlt="Athletic Store"
+                     projectName="Athletic Store"
+                     projectDescription="An Athletic Store to browse items from an API built and hosted by me."
+                     link="http://www.chaseburr.com/projects/athletic-shop"
+                     language="ReactJS"
+                  />
+                  <Project
+                     imgSrc="/images/gameoflife.png"
+                     imgAlt="Conway's Game of Life"
+                     projectName="Conway's Game of Life"
+                     projectDescription="This was a group project at Weber State University with 2 other students."
+                     link="/projects/"
+                     language="C# / JavaScript"
+                  />
+                  <Project
+                     imgSrc="/images/giphy.png"
+                     imgAlt="Giphy"
+                     projectName="Giphy Project"
+                     projectDescription="This project uses the GIPHY API to grab gifs."
+                     link="https://chaseburr.github.io/giphy-search/"
+                     language="JavaScript"
+                  />
+               </ul>
+            </div>
          </div>
-         <div className="technical-section">
-            <h1 className="section-header">Technical Abilities</h1>
-            <ul>
-               <div className="languages">
-                  <ul>
-                     <h2>Languages</h2>
-                     <li>JavaScript</li>
-                     <li>HTML</li>
-                     <li>CSS</li>
-                     <li>C++</li>
-                     <li>C#</li>
-                     <li>Java</li>
-                  </ul>
-               </div>
-            </ul>
-         </div>
-         <Footer />
       </>
    );
 }
--- src/components/hero/Hero.js
+++ src/components/hero/Hero.js
@@ -2,24 +2,13 @@ import React from "react";
 import "./Hero.css";
 
 function Hero() {
-  return (
-    <div className="hero-container">
-      <div className="profile-picture">
-        <img src="/images/avatar.jpg" alt="chase burr" />
+   return (
+      <div className="hero">
+         <div className="hero-container">
+            <h1>Chase Burr's Portfolio</h1>
+         </div>
       </div>
-      <div className="about-container">
-        <h1>Hi! I'm Chase Burr</h1>
-        <h2>Software Engineer</h2>
-        <p>
-          Computer Science graduate seeking to improve my skills as a software
-          engineer. Strong foundation in Object Oriented programming and
-          troubleshooting. Quick learner and ability to adapt to new languages.
-          I may not have much experience job wise, but I will dedicate a lot of
-          time learning and adapting to new environments.
-        </p>
-      </div>
-    </div>
-  );
+   );
 }
 
 export default Hero;
